fa7503a9465fb4670f80c073cfbd90108d6cdd44
annagrinkotest <64903335+annagrinkotest@users.noreply.github.com>
2020-06-13 20:32:22 +0300

    Add  corrected file

--- main.js
+++ main.js
@@ -1,29 +1,65 @@
 document.addEventListener('DOMContentLoaded', () => {
+  var lastValClick;
   var currentVal;
-  document.querySelector(".app-keys").addEventListener("click", () => {
+  var buttonClick = document.querySelectorAll('.key');
+for(var i = 0; i < buttonClick.length; i++)
+ {
+  buttonClick[i].addEventListener("click", () => {
     var target = event.target;
-    if (target.innerHTML != "AC") {
-      if (target.innerHTML == "=") {
-        currentVal = Math.round(eval(currentVal)*1000)/1000;
-        document.querySelector("#app-display").textContent = currentVal;
+    
+    if (target.innerHTML !== "AC") {
+      if (target.innerHTML === "=") {
+        currentVal = Math.round(eval(currentVal) * 1000) / 1000;
+        document.querySelector(".app-display").textContent = currentVal;
+        lastValClick = target.innerHTML;
       }
       else {
-        if (document.querySelector("#app-display").textContent == 0) { document.querySelector("#app-display").textContent = target.innerHTML;
-            currentVal = target.innerHTML;
-           }
+        if (document.querySelector(".app-display").textContent === "0") {
+          document.querySelector(".app-display").textContent = target.innerHTML;
+          currentVal = target.innerHTML;
+          lastValClick = target.innerHTML;
+        }
         else {
-          if (target.innerHTML == "÷") { 
-            currentVal += "/";
-            document.querySelector("#app-display").textContent += target.innerHTML; }
-          else if (target.innerHTML == "×") { 
-            currentVal += "*";
-            document.querySelector("#app-display").textContent += target.innerHTML; }
+          if (target.innerHTML === "÷") {
+            if (lastValClick !== "÷") {
+              lastValClick = target.innerHTML;
+              currentVal += "/";
+              document.querySelector(".app-display").textContent += target.innerHTML;
+            }
+          }
+          else if (target.innerHTML === "×") {
+            if (lastValClick !== "×") {
+              lastValClick = target.innerHTML;
+              currentVal += "*";
+              document.querySelector(".app-display").textContent += target.innerHTML;
+            }
+          }
+          else if (target.innerHTML === "+") {
+            if (lastValClick !== "+") {
+              lastValClick = target.innerHTML;
+              currentVal += "+";
+              document.querySelector(".app-display").textContent += target.innerHTML;
+            }
+          }
+          else if (target.innerHTML === "-") {
+            if (lastValClick !== "-") {
+              lastValClick = target.innerHTML;
+              currentVal += "-";
+              document.querySelector(".app-display").textContent += target.innerHTML;
+            }
+          }
           else {
-            currentVal += target.innerHTML; 
-            document.querySelector("#app-display").textContent += target.innerHTML; }
+            lastValClick = target.innerHTML;
+            currentVal += target.innerHTML;
+            document.querySelector(".app-display").textContent += target.innerHTML;
+          }
         }
       }
     }
-    else { document.querySelector("#app-display").textContent = "0"; }
+    else {
+      lastValClick = target.innerHTML;
+      document.querySelector(".app-display").textContent = "0";
+    }
   });
+}
 });
